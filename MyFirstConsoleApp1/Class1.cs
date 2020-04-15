01bc60f64ee72275be36ea2e0269d689096cc771
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-04-15 13:37:21 +0200

    Add project files.

---
+++ MyFirstConsoleApp1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstConsoleApp1
+{
+    class Person
+    {
+        private string _name;
+        private int _age;
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+        public string name
+        {
+            get { return _name; }
+        }
+        public int age
+        {
+            get { return _age; }
+        }
+    }
+}
---
+++ MyFirstConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Person Person1 = new Person("Simon", 23);
+            Person Person2 = new Person("Bob", 69);
+            Console.WriteLine($"Den første person hedder {Person1.name}, og er {Person1.age} år gammel");
+            Console.WriteLine($"Den anden person hedder {Person2.name}, og er {Person2.age} år gammel");
+        }
+    }
+}
